0d895faa56131ea3541815fe9e03fdf63c335d9f
Adwait Vaidya <42444296+AdwaitVaidya@users.noreply.github.com>
2023-05-25 08:45:45 -0500

    c

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,11 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginScreen from "./components/LoginScreen";
 import SearchPage from "./components/SearchPage";
-import AppContext from "./AppContext";
 
 const App: React.FC = () => {
   const [authenticated, setAuthenticated] = useState(false);
 
-  const sharedData = {
-    theme: "dark",
-    user: {
-      name: "John Doe",
-      email: "john.doe@example.com",
-    },
-  };
-
   return (
-    <AppContext.Provider value={sharedData.theme}>
       <Routes>
         <Route
           path="/"
@@ -24,7 +14,6 @@ const App: React.FC = () => {
         />
         <Route path="/search" element={<SearchPage />} />
       </Routes>
-    </AppContext.Provider>
   );
 };
 
